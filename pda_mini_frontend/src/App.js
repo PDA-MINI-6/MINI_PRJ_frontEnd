a71083500c869fb093c085e9db87d8c145e4efed
wlsdk9803 <wlsdk9803@naver.com>
2024-05-21 16:04:28 +0900

    feat: App.js bootstrap 적용하여 화면 분할

--- pda_mini_frontend/src/App.js
+++ pda_mini_frontend/src/App.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import PopupSearch from "./component/PopupSearch";
 import PopupMap from "./component/PopupMap";
 import PopupDetail from "./component/PopupDetail";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
     <>
-      {isClicked ? (
-        <PopupDetail isClicked={isClicked} />
-      ) : (
-        <PopupSearch isClicked={isClicked} />
-      )}
-      <PopupMap />
+      <Container>
+        <Row>
+          <Col xs={6} sm={6}>
+            {isClicked ? (
+              <PopupDetail isClicked={isClicked} />
+            ) : (
+              <PopupSearch isClicked={isClicked} />
+            )}
+          </Col>
+          <Col xs={6} sm={6}>
+            <PopupMap />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
