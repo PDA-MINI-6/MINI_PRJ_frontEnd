3e1b9a8c52cbcb58f3433751275cee391e769328
yoonjaeuk <jasper09@ajou.ac.kr>
2024-05-27 11:23:33 +0900

    Revert "Feat.상세페이지 추가"
    This reverts commit e40333019f0abc3842929278bcbd5c9f0a3436b8.

--- src/component/PopupMap.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PopupMap = () => {
-  return <div></div>;
-};
-
-export default PopupMap;
--- src/component/PopupSearch.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PopupSearch = () => {
-  return <div></div>;
-};
-
-export default PopupSearch;
