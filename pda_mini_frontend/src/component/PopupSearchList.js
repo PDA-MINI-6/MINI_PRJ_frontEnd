5a2693d157247bdc7beeae80401e4c12aa6f43cf
wlsdk9803 <wlsdk9803@naver.com>
2024-05-27 14:09:35 +0900

    feat: 메인페이지 리스트 스크롤 추가

--- pda_mini_frontend/src/component/PopupSearchList.js
+++ pda_mini_frontend/src/component/PopupSearchList.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
-import Badge from "react-bootstrap/Badge";
 import ListGroup from "react-bootstrap/ListGroup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import PopupSearchLiked from "./PopupSearchLiked";
 import { naverMapContext } from "./NaverMapProvider";
+import "./popupSearch.css";
 
 const PopupSearchList = ({ sortOption, searchText, setSearchText }) => {
   const navigate = useNavigate();
@@ -34,48 +34,52 @@ const PopupSearchList = ({ sortOption, searchText, setSearchText }) => {
   });
 
   return (
-    <ListGroup as="ol" style={{ margin: "10px 0 20px 0" }}>
-      {sortedList.map((elem) => {
-        return searchText === "" ||
-          elem.tags.some((tag) => tag.includes(searchText)) ||
-          elem.title.includes(searchText) ||
-          elem.subtitle.includes(searchText) ? (
-          <ListGroup.Item
-            key={elem.id}
-            action
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-            style={{ cursor: "pointer" }}
-            onClick={() => navigate(`/${elem.id}`)}
-          >
-            <div>
-              <img
-                src={elem.images[0]}
-                style={{
-                  width: "100px",
-                  height: "80px",
-                  objectFit: "contain",
-                }}
+    <div id="container">
+      <ListGroup as="ol">
+        {sortedList.map((elem) => {
+          return searchText === "" ||
+            elem.tags.some((tag) => tag.includes(searchText)) ||
+            elem.title.includes(searchText) ||
+            elem.subtitle.includes(searchText) ? (
+            <ListGroup.Item
+              key={elem.id}
+              action
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+              style={{ cursor: "pointer" }}
+              onClick={() => navigate(`/${elem.id}`)}
+            >
+              <div>
+                <img
+                  src={elem.images[0]}
+                  style={{
+                    width: "100px",
+                    height: "80px",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+              <div className="ms-2 me-auto">
+                <div className="fw-bold" style={{ margin: "2px 0" }}>
+                  {elem.title}
+                </div>
+                "{elem.subtitle}"
+                <div style={{ fontSize: "12px", margin: "6px" }}>
+                  {elem.startDate} ~ {elem.endDate}
+                </div>
+              </div>
+              <PopupSearchLiked
+                initialLiked={
+                  localStorage.getItem(`liked_${elem.id}`) === "true"
+                }
+                initialLikes={elem.liked}
+                id={elem.id}
               />
-            </div>
-            <div className="ms-2 me-auto">
-              <div className="fw-bold" style={{ margin: "2px 0" }}>
-                {elem.title}
-              </div>
-              "{elem.subtitle}"
-              <div style={{ fontSize: "12px", margin: "6px" }}>
-                {elem.startDate} ~ {elem.endDate}
-              </div>
-            </div>
-            <PopupSearchLiked
-              initialLiked={localStorage.getItem(`liked_${elem.id}`) === "true"}
-              initialLikes={elem.liked}
-              id={elem.id}
-            />
-          </ListGroup.Item>
-        ) : null;
-      })}
-    </ListGroup>
+            </ListGroup.Item>
+          ) : null;
+        })}
+      </ListGroup>
+    </div>
   );
 };
 
