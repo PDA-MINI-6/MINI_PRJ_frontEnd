97514fcb7faa0ce20f6cce97fa489b6ab640d59a
yapyap2 <wonwoo42@gmail.com>
2024-05-27 11:17:28 +0900

    feat: PopupMap 엘리먼트 추가

--- pda_mini_frontend/src/component/PopupMap.js
+++ pda_mini_frontend/src/component/PopupMap.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { naverMapContext } from "./NaverMapProvider";
 
-const PopupMap = () => {
-  return <div></div>;
-};
-
-export default PopupMap;
+export default function PopupMap() {
+  const { initMap } = useContext(naverMapContext);
+  const ref = useRef();
+  useEffect(() => {
+    initMap(ref.current);
+  }, []);
+  return (
+    <>
+      <div ref={ref} style={{ height: "100%" }}></div>
+    </>
+  );
+}
