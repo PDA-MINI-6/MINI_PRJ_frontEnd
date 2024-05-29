4dff110960e12ae01f702ad5ea6c03e3af21df87
yapyap2 <wonwoo42@gmail.com>
2024-05-29 12:31:21 +0900

    PopupDetail에서 initMap, initMarker 호출 로직 추가

--- pda_mini_frontend/src/component/PopupDetail.js
+++ pda_mini_frontend/src/component/PopupDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import PopupDetailComment from "./PopupDetailComment/PopupDetailComment";
 import { Container, Row } from "react-bootstrap";
 import Root from "./PopupDetail/Root";
@@ -6,13 +6,15 @@ import "./PopupDetail.css";
 import { useParams } from "react-router-dom";
 import { REQUEST_URL } from "../constant";
 import axios from "axios";
+import { naverMapContext } from "./NaverMapProvider";
 
 const PopupDetail = () => {
+  const { ready, initMap, initMarker } = useContext(naverMapContext);
+
   const { id } = useParams();
   const [popupData, setPopupData] = useState(null);
 
   useEffect(() => {
-    console.log("test");
     axios
       .get(`${REQUEST_URL}/${id}`)
       .then((response) => {
@@ -21,6 +23,13 @@ const PopupDetail = () => {
       .catch((error) => {
         console.error("Error fetching popup detail:", error);
       });
+
+    if (!ready.current) {
+      initMap();
+      const localData = localStorage.getItem("searchList");
+      initMarker(JSON.parse(localData));
+      console.log("맵 리로드");
+    }
   }, [id]);
 
   return (
